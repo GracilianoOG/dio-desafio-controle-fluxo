515a7d71222f0d9674841c874caab6d60c864333
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-04 22:57:48 -0300

    Add scanner and inputs

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int primeiroParametro = scanner.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        int segundoParametro = scanner.nextInt();
     }
 }
